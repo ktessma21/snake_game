97dafb6deb9dc734ea1b59922b8a3f264b1a657b
ktessma21 <142500915+ktessma21@users.noreply.github.com>
2024-07-10 20:47:34 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.speed("fastest")
         self.hideturtle()
         self.goto(0, 270)
@@ -12,12 +13,14 @@ class Scoreboard(Turtle):
 
     def display_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Verdana", 15, "normal"))
+        self.write(f"Score: {self.score} Highscore: {self.high_score}", align="center", font=("Verdana", 15, "normal"))
 
     def increment_score(self):
         self.score += 1
         self.display_score()
 
     def game_over(self):
-        self.home()
-        self.write("Game Over.", align="center", font=("Verdana", 15, "normal"))
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.display_score()
