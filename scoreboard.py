e37fe8942b9c0ff95a610342e257492014a86dcd
ktessma21 <142500915+ktessma21@users.noreply.github.com>
2024-07-12 19:39:37 +0300

    Update scoreboard.py to save the highest score a user get

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.speed("fastest")
         self.hideturtle()
         self.goto(0, 270)
@@ -22,5 +23,7 @@ class Scoreboard(Turtle):
     def game_over(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode = "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.display_score()
